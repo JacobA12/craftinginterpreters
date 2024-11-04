6555c785c9c7298138e4a0980fa1487db27e1d74
Jacob Adams big-boi <jacob.adams121202@gmail.com>
2024-11-04 11:29:25 -0700

    scan tokens function

--- java/com/craftinginterpreters/lox/Scanner.java
+++ java/com/craftinginterpreters/lox/Scanner.java
@@ -1,17 +1,29 @@
 package com.craftinginterpreters.lox;
 
+import static com.craftinginterpreters.lox.TokenType.*;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static com.craftinginterpreters.lox.TokenType.*;
+class Scanner {
 
-class Scanner{
   private final String source;
   private final List<Token> tokens = new ArrayList<>();
 
-  Scanner(String source){
+  Scanner(String source) {
     this.source = source;
+  }
+
+  List<Token> scanTokens() {
+    while (!isAtEnd()) {
+      //We are at the beginning of the next lexeme/
+      start = current;
+      scanToken();
+    }
+
+    tokens.add(new Token(EOF, "", null, line));
+    return tokens;
   }
 }
