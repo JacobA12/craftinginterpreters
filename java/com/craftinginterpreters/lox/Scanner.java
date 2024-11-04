d1a1e48453c4360253c0f832ae15b22931b624d1
Jacob Adams big-boi <jacob.adams121202@gmail.com>
2024-11-04 11:48:37 -0700

    comments

--- java/com/craftinginterpreters/lox/Scanner.java
+++ java/com/craftinginterpreters/lox/Scanner.java
@@ -10,8 +10,17 @@ import java.util.Map;
 class Scanner {
 
   private final String source;
+  //list to fill with tokens
   private final List<Token> tokens = new ArrayList<>();
+  //start and current fields are offsets that index into the string
+  //start points to the first character in the lexeme being scanned
+  private int start = 0;
+  //current points at the character currently being considered
+  private int current = 0;
+  //line tracks what source line current is on so we can produce tokens that known thier locations
+  private int line = 1;
 
+  //stores raw source code as a simple string
   Scanner(String source) {
     this.source = source;
   }
@@ -25,5 +34,58 @@ class Scanner {
 
     tokens.add(new Token(EOF, "", null, line));
     return tokens;
+  }
+
+  private void scanToken() {
+    char c = advance();
+    switch (c) {
+      case '(':
+        addToken(LEFT_PAREN);
+        break;
+      case ')':
+        addToken(RIGHT_PAREN);
+        break;
+      case '{':
+        addToken(RIGHT_BRACE);
+        break;
+      case '}':
+        addToken(LEFT_BRACE);
+        break;
+      case ',':
+        addToken(COMMA);
+        break;
+      case '.':
+        addToken(DOT);
+        break;
+      case '-':
+        addToken(MINUS);
+        break;
+      case '+':
+        addToken(PLUS);
+        break;
+      case ';':
+        addToken(SEMICOLON);
+        break;
+      case '*':
+        addToken(STAR);
+        break;
+    }
+  }
+
+  private boolean isAtEnd() {
+    return current >= source.length();
+  }
+
+  private char advance() {
+    return source.charAt(current++);
+  }
+
+  private void addToken(TokenType type) {
+    addToken(type, null);
+  }
+
+  private void addToken(TokenType type, Object literal) {
+    String text = source.substring(start, current);
+    tokens.add(new Token(type, text, literal, line));
   }
 }
